602fedabd6f2a3b6c5fd3f47d59913e251413ac9
Phornhathai <phornhathai.aum@gmail.com>
2026-02-19 00:45:42 +0700

    init: set up project and initial project with react-router using browserRouter

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router";
+
+import HomePage from "./pages/home";
+import DetailPage from "./pages/detail";
+import { RouterProvider } from "react-router/dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      index: true, // index: true means this route will be rendered when the path is exactly "/"s
+      element: <HomePage />,
+    },
+    {
+      path: "/detail",
+      element: <DetailPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
