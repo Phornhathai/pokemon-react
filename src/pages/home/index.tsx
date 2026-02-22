602b73b2f624e2bd5a7ca328189628d953e6c405
Phornhathai <phornhathai.aum@gmail.com>
2026-02-22 18:49:21 +0700

    feat: test function call data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { pokemonDetailService, pokemonListService } from "@/services";
+import { useEffect } from "react";
+import logo from "@/images/logo.webp";
+import SearchForm from "@/components/SearchForm";
 
 export default function HomePage() {
+  const fetchPokemonList = async () => {
+    const response = await pokemonListService.getPokemonList();
+    console.log(response.data);
+  };
+
+  const fetchPokemonDetail = async () => {
+    const response = await pokemonDetailService.getPokemonDetail("ditto");
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchPokemonList();
+    fetchPokemonDetail();
+  }, []);
+
   return (
-    <div>
-      
+    <div className="w-[90%] m-auto max-w-275">
+      <div className="flex justify-center items-center">
+        <img src={logo} className="max-h-20 mt-5" alt="logo" />
+      </div>
+      <SearchForm />
     </div>
-  )
+  );
 }
